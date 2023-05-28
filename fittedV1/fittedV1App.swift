f4338916aea21251ae10139ab9c901ebc1b508ae
adam rajkotwala <adamraj03@gmail.com>
2023-05-27 20:06:59 -0400

    Initial Commit

---
+++ fittedV1/fittedV1App.swift
@@ -0,0 +1,17 @@
+//
+//  fittedV1App.swift
+//  fittedV1
+//
+//  Created by Adam Raj on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct fittedV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fittedV1UITests/fittedV1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fittedV1UITestsLaunchTests.swift
+//  fittedV1UITests
+//
+//  Created by Adam Raj on 5/27/23.
+//
+
+import XCTest
+
+final class fittedV1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
